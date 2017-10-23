329b2113175871125f8fee871bd41d479e564059
kevingreer <kevingreer@me.com>
2017-10-22 21:55:48 -0400

    connection pool

--- appdev/connectors/mysql_connector.py
+++ appdev/connectors/mysql_connector.py
@@ -1,16 +1,17 @@
 from threading import Thread
-from Queue import Queue
+from Queue import Queue, Empty
 import mysql.connector
 
 
 class DbThread(Thread):
-  def __init__(self, connection, cursor, table, data, queue=None):
+  def __init__(self, connection_pool, table, input_queue, result_queue=None,
+               queue_timeout=0.5):
     super(DbThread, self).__init__()
-    self.connection = connection
-    self.cursor = cursor
+    self.connection_pool = connection_pool
     self.table = table
-    self.data = data
-    self.queue = queue
+    self.input_queue = input_queue
+    self.result_queue = result_queue
+    self.queue_timeout = queue_timeout
 
 
 class WriterThread(DbThread):
@@ -18,56 +19,87 @@ class WriterThread(DbThread):
     # Create query in the format:
     #   "INSERT INTO salaries (emp_no, salary, from_date, to_date)
     #     VALUES (%(emp_no)s, %(salary)s, %(from_date)s, %(to_date)s)"
-    values_string = ['%({})s'.format(col) for col in self.data.keys()]
-    insert_query = "INSERT INTO {} ({}) VALUES ({})" \
-      .format(self.table, ', '.join(self.data.keys()), ', '.join(values_string))
-    self.cursor.execute(insert_query, self.data)
-    self.connection.commit()
+    # data is dictionary such as {'emp_no': 1, 'salary': 2, ...}
+    while not self.input_queue.empty():
+      connection = self.connection_pool.get()
+      cursor = connection.cursor()
+      try:
+        # A thread may enter the loop, but another thread could pluck the
+        # last item. This timeout is to prevent a thread from waiting for
+        # another item that will never come.
+        data = self.input_queue.get(timeout=self.queue_timeout)
+      except Empty:
+        break
+      values_string = ['%({})s'.format(col) for col in data.keys()]
+      insert_query = "INSERT INTO {} ({}) VALUES ({})" \
+        .format(self.table, ', '.join(data.keys()),
+                ', '.join(values_string))
+      cursor.execute(insert_query, data)
+      connection.commit()
+      self.input_queue.task_done()
+      self.connection_pool.put(connection)
 
 
 class ReaderThread(DbThread):
   def run(self):
-    # data is (a, b) which indicates the inclusive range for this query
-    read_query = "SELECT * FROM {0} OFFSET {1} LIMIT {2} - {1} + 1" \
-                    .format(self.table, self.data[0], self.data[1])
-    self.cursor.execute(read_query)
-    return [_ for _ in self.cursor]
+    while not self.input_queue.empty():
+      connection = self.connection_pool.get()
+      cursor = connection.cursor()
+      try:
+        a, b = self.input_queue.get(timeout=self.queue_timeout)
+      except Empty:
+        break
+      read_query = "SELECT * FROM {} LIMIT {} OFFSET {}" \
+                     .format(self.table, b-a+1, a)
+      cursor.execute(read_query)
+      result = []
+      for row in cursor:
+        result.append(row)
+      self.input_queue.task_done()
+      self.result_queue.put(result)
+      self.connection_pool.put(connection)
 
 
-class MySQLConnector(Object):
-  def __init__(self, user, password, host, database):
-    self.connection = mysql.connector.connect(user=user,
-                                              password=password,
-                                              host=host,
-                                              database=database)
-    self.cursor = self.connection.cursor()
+class MySQLConnector(object):
+  def __init__(self, user, password, host, database,
+               num_connections=10, num_threads=10):
+    self.config = {
+        'user': user,
+        'password': password,
+        'host': host,
+        'database': database
+    }
+    self.connection_pool = Queue()
+    self.num_threads = num_threads
+    for _ in range(num_connections):
+      cnx = mysql.connector.connect(**self.config)
+      self.connection_pool.put(cnx)
 
   def close(self):
-    self.connection.close()
+    while not self.connection_pool.empty():
+      connection = self.connection_pool.get()
+      connection.close()
 
-  def read_batch(self, table, start, end, num_workers):
-    threads = []
-    queue = Queue()
-    range_size = (end - start + 1) / num_workers
-    for i in num_workers:
-      a = range_size * i
-      b = a + range_size - 1
-      thread = ReaderThread(self.connection, self.cursor,
-                            table, (a, b), queue=queue)
-      thread.start()
-      threads.append(thread)
-    for thread in threads:
-      thread.join()
+  def read_batch(self, table, start, end, interval_size=10):
+    input_queue = Queue()
+    result_queue = Queue()
+    a = start
+    while a <= end:
+      input_queue.put((a, a + interval_size))
+      a += interval_size
+    for _ in range(self.num_threads):
+      ReaderThread(self.connection_pool, table, input_queue,
+                   result_queue=result_queue).start()
+    input_queue.join()
     rows = []
-    while not queue.empty():
-      rows.extend(queue.get())
+    while not result_queue.empty():
+      rows.extend(result_queue.get())
     return rows
 
   def write_batch(self, table, rows):
-    threads = []
-    for i in range(rows):
-      thread = WriterThread(self.connection, self.cursor, table, rows[i])
-      thread.start()
-      threads.append(thread)
-    for thread in threads:
-      thread.join()
+    input_queue = Queue()
+    for row in rows:
+      input_queue.put(row)
+    for _ in range(self.num_threads):
+      WriterThread(self.connection_pool, table, input_queue).start()
+    input_queue.join()
